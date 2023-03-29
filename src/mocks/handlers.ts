6d5d3c03fe9f881d5f7fac515efb223d36ee1645
Xyphuz <hansen0715.2@gmail.com>
2023-03-29 17:04:09 +0800

    chore: add mock api for user

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,4 +1,6 @@
 import { rest } from 'mswx';
+import { UserSchema } from '../models/user';
+import { ZodError } from 'zod';
 
 rest.config.API_PREFIX = '/api/v1';
 
@@ -16,16 +18,116 @@ const AuthMiddleware = rest.middleware((req, res, ctx, next) => {
 	return next();
 });
 
-const userHandlers = [
-	rest.get('/user', (req, res, ctx) => {
+const userWithoutAuthorizationHandlers = [
+	rest.post('/user', async (req, res, ctx) => {
+		const payload = await req.json();
+
+		try {
+			UserSchema.parse(payload);
+
+			return res(ctx.status(201));
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return res(
+					ctx.status(400),
+					ctx.json({
+						message: 'Bad request',
+						errors: error.errors
+					})
+				);
+			}
+
+			return res(
+				ctx.status(500),
+				ctx.json({
+					message: 'Internal server error'
+				})
+			);
+		}
+	}),
+	rest.post('/user/login', async (req, res, ctx) => {
+		const payload = await req.json();
+
+		try {
+			UserSchema.parse(payload);
+
+			return res(ctx.status(200));
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return res(
+					ctx.status(400),
+					ctx.json({
+						message: 'Bad request',
+						errors: error.errors
+					})
+				);
+			}
+
+			return res(
+				ctx.status(500),
+				ctx.json({
+					message: 'Internal server error'
+				})
+			);
+		}
+	})
+];
+
+const userWithAuthorizationHandlers = [
+	rest.post('/user/logout', async (req, res, ctx) => {
+		return res(ctx.status(200));
+	}),
+	rest.get('/user/:username', async (req, res, ctx) => {
+		const { username } = req.params;
+
 		return res(
 			ctx.status(200),
 			ctx.json({
 				id: 1,
-				name: 'John Doe'
+				username,
+				firstName: 'Test',
+				lastName: 'User',
+				email: 'test@gmail.com',
+				password: 'test',
+				phone: '1234567890',
+				userStatus: 1
 			})
 		);
+	}),
+	rest.patch('/user/:username', async (req, res, ctx) => {
+		try {
+			UserSchema.parse(await req.json());
+
+			return res(ctx.status(200));
+		} catch (error) {
+			if (error instanceof ZodError) {
+				return res(
+					ctx.status(400),
+					ctx.json({
+						message: 'Bad request',
+						errors: error.errors
+					})
+				);
+			}
+		}
+	}),
+	rest.delete('/user/:username', async (req, res, ctx) => {
+		const { username } = req.params;
+
+		if (username !== 'test') {
+			return res(
+				ctx.status(404),
+				ctx.json({
+					message: 'User not found'
+				})
+			);
+		}
+
+		return res(ctx.status(200));
 	})
 ];
 
-export const handlers = [...userHandlers.map(AuthMiddleware)];
+export const handlers = [
+	...userWithoutAuthorizationHandlers,
+	...userWithAuthorizationHandlers.map(AuthMiddleware)
+];
---
+++ src/models/user.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+
+export const UserSchema = z.object({
+	id: z.number(),
+	username: z.string(),
+	firstName: z.string(),
+	lastName: z.string(),
+	email: z.string(),
+	password: z.string(),
+	phone: z.string(),
+	userStatus: z.number()
+});
+
+export type User = z.infer<typeof UserSchema>;
