22607d27bff39488b9c5f774faacfdd19590229b
Xyphuz <hansen0715.2@gmail.com>
2023-04-19 18:05:29 +0800

    chore: add event page

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -4,7 +4,7 @@
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
 	<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-		<a href="https://flowbite.com/" class="flex items-center">
+		<a href="/" class="flex items-center">
 			<img
 				src="https://www.svgrepo.com/show/498281/people.svg"
 				class="h-8 mr-3"
---
+++ src/routes/events/[id]/+page.svelte
@@ -0,0 +1,18 @@
+<div class="mx-12 md:mx-36 mt-8 mb-12 flex flex-col gap-6">
+	<div class="font-bold text-4xl">Lorem Ipsum</div>
+
+	<div class="text-gray-400">2023-01-01 00:00:00</div>
+
+	<div class="border-b border-gray-200 dark:border-gray-700" />
+
+	<div class="leading-8 mt-4">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, est enim eum recusandae adipisci
+		similique iste voluptas assumenda dolores, officia non dicta labore voluptatum odit
+		exercitationem doloremque dolor id. Id. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+		Eius, est enim eum recusandae adipisci similique iste voluptas assumenda dolores, officia non
+		dicta labore voluptatum odit exercitationem doloremque dolor id. Id. Lorem ipsum dolor sit amet
+		consectetur adipisicing elit. Eius, est enim eum recusandae adipisci similique iste voluptas
+		assumenda dolores, officia non dicta labore voluptatum odit exercitationem doloremque dolor id.
+		Id.
+	</div>
+</div>
