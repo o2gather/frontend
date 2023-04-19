18b3e6919e81958c396ef9798ec206df854dc35d
Xyphuz <hansen0715.2@gmail.com>
2023-04-19 18:59:49 +0800

    chore: stop text propagation in card

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -13,10 +13,18 @@
 		alt=""
 	/>
 	<div class="p-5">
-		<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+		<h5
+			class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white cursor-auto"
+			on:click|stopPropagation
+			on:keypress|stopPropagation
+		>
 			Noteworthy technology acquisitions 2021
 		</h5>
-		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+		<p
+			class="mb-3 font-normal text-gray-700 dark:text-gray-400 cursor-auto"
+			on:click|stopPropagation
+			on:keypress|stopPropagation
+		>
 			Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
 			chronological order.
 		</p>
