58278b85fa15adde94523ef0622bfd9a5feee678
laijunbin <xyz607xx@gmail.com>
2023-06-03 23:40:48 +0800

    chore: change category input to native

--- src/routes/events/create/+page.svelte
+++ src/routes/events/create/+page.svelte
@@ -10,8 +10,6 @@
 
 	const { form, errors, constraints } = superForm(data.form);
 
-	let isDropdownOpen = false;
-
 	let startTime = isoToDateTimeString(new Date().getTime());
 	let endTime = isoToDateTimeString(new Date().getTime());
 
@@ -53,73 +51,33 @@
 						for="search-dropdown"
 						class="sr-only mb-2 text-sm font-medium text-gray-900 dark:text-white"
 					/>
+					<input
+						class="z-10 inline-flex flex-shrink-0 items-center rounded-l-lg border border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none dark:border-gray-700 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+						list="categories"
+						name="category"
+						bind:value={$form.category}
+						{...$constraints.category}
+					/>
+					<datalist id="categories">
+						{#each data.categories as category}
+							<option value={category} />
+						{/each}
+					</datalist>
 
-					<button
-						id="dropdown-button"
-						on:click={() => (isDropdownOpen = !isDropdownOpen)}
-						on:keypress={() => (isDropdownOpen = !isDropdownOpen)}
-						class="z-10 inline-flex flex-shrink-0 items-center rounded-l-lg border border-gray-300 bg-gray-100 px-4 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gray-200 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:border-gray-700 dark:bg-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-						type="button"
-					>
-						{$form.category || 'All categories'}
-						<svg
-							aria-hidden="true"
-							class="ml-1 h-4 w-4"
-							fill="currentColor"
-							viewBox="0 0 20 20"
-							xmlns="http://www.w3.org/2000/svg"
-							><path
-								fill-rule="evenodd"
-								d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-								clip-rule="evenodd"
-							/></svg
-						>
-						<select name="category" required class="pointer-events-none opacity-0">
-							<option value={$form.category}>{$form.category}</option>
-						</select>
-					</button>
-					<div
-						class="absolute z-10 w-44 translate-y-[50px] divide-y divide-gray-100 rounded-lg bg-white shadow dark:bg-gray-700"
-						class:hidden={!isDropdownOpen}
-					>
-						<ul
-							class="py-2 text-sm text-gray-700 dark:text-gray-200"
-							aria-labelledby="dropdown-button"
-						>
-							{#each data.categories as category (category)}
-								<li
-									class="block cursor-pointer px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-									on:click={() => {
-										$form.category = category;
-										isDropdownOpen = false;
-									}}
-									on:keypress={() => {
-										$form.category = category;
-										isDropdownOpen = false;
-									}}
-								>
-									{category}
-								</li>
-							{/each}
-						</ul>
-					</div>
-
-					<div class="relative w-full">
-						<input
-							type="text"
-							id="search-dropdown"
-							class="z-20 block w-full rounded-r-lg border border-l-2 border-gray-300 border-l-gray-100 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
-							placeholder="Event name"
-							required
-							bind:value={$form.name}
-							{...$constraints.name}
-							on:input={() => {
-								superValidate($form, schemas.createEvent_Body).then((result) => {
-									$errors.name = result.errors.name;
-								});
-							}}
-						/>
-					</div>
+					<input
+						type="text"
+						id="search-dropdown"
+						class="z-20 block w-full rounded-r-lg border border-l-2 border-gray-300 border-l-gray-100 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500"
+						placeholder="Event name"
+						required
+						bind:value={$form.name}
+						{...$constraints.name}
+						on:input={() => {
+							superValidate($form, schemas.createEvent_Body).then((result) => {
+								$errors.name = result.errors.name;
+							});
+						}}
+					/>
 				</div>
 
 				<ErrorMessage message={$errors.name} />
