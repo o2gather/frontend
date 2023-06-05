1883c4a22b27ae03dcb8226ac8a840473e222e9e
laijunbin <xyz607xx@gmail.com>
2023-06-05 10:56:44 +0800

    chore: remove join button

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -12,17 +12,11 @@
 	class="h-full cursor-pointer rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800"
 >
 	<div class="flex h-full flex-col p-8">
-		<h5
-			class="mb-8 cursor-auto text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-			on:click|stopPropagation
-			on:keypress|stopPropagation
-		>
+		<h5 class="mb-8 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 			{title}
 		</h5>
 		<p
-			class="mb-10 line-clamp-2 grow cursor-auto whitespace-pre-wrap font-normal text-gray-600 dark:text-gray-400"
-			on:click|stopPropagation
-			on:keypress|stopPropagation
+			class="mb-10 line-clamp-2 grow whitespace-pre-wrap font-normal text-gray-600 dark:text-gray-400"
 		>
 			{description}
 		</p>
@@ -33,15 +27,6 @@
 				Read more
 
 				<img src="/arrow.svg" class="-mr-1 ml-2 h-4 w-4" alt="arrow" />
-			</div>
-			<div class="grow text-right">
-				<div
-					class="inline-flex items-center rounded-lg bg-green-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-				>
-					Join
-
-					<img src="/plus.svg" class="-mr-1 ml-2 h-4 w-4" alt="plus" />
-				</div>
 			</div>
 		</div>
 	</div>
