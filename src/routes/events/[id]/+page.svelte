367607f0f3f0c1de01d41d105315a36d3fed2a00
laijunbin <xyz607xx@gmail.com>
2023-06-05 10:51:01 +0800

    chore: update event layout

--- src/routes/events/[id]/+page.svelte
+++ src/routes/events/[id]/+page.svelte
@@ -47,10 +47,13 @@
 	});
 </script>
 
-<div class="mx-12 mb-12 mt-8 flex flex-col gap-6 md:mx-36">
+<div class="mx-12 mb-6 mt-8 flex flex-col gap-6 md:mx-36">
 	<div class="flex flex-wrap items-center justify-between text-4xl font-bold">
 		<div class="flex flex-wrap items-center">
-			<div class="mr-4">{event.name}</div>
+			<div class="mr-4 flex flex-wrap items-center">
+				<div class="mr-4">{event.name}</div>
+				<div class="text-lg">({event.min_amount}人 - {event.max_amount}人)</div>
+			</div>
 			{#if !isOwner}
 				{#if isMember}
 					<button
@@ -158,6 +161,75 @@
 		{/if}
 	</div>
 
+	<div class="flex flex-wrap items-center text-gray-700">
+		{#if isOwner}
+			<details class="w-full">
+				<summary>
+					當前參與人數：{event.members_count + 1}人
+
+					{#if event.members_count + 1 >= event.min_amount}
+						<button
+							class="m-2 inline-flex cursor-pointer items-center rounded-lg bg-green-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+							on:click={() => {
+								Swal.fire({
+									title: '確定成團嗎?',
+									text: '成團後成員將無法加入或退出',
+									icon: 'question',
+									confirmButtonText: 'Yes',
+									cancelButtonText: 'No',
+									showCancelButton: true
+								}).then((result) => {
+									if (result.isConfirmed) {
+										// api.loading
+										// 	.finishEvent(undefined, {
+										// 		params: {
+										// 			eventId: event.id
+										// 		},
+										// 		withCredentials: true
+										// 	})
+										// 	.then(() => {
+										Swal.fire({
+											title: '成團成功!',
+											icon: 'success',
+											confirmButtonText: 'OK'
+										});
+										// })
+										// .catch((err) => {
+										// 	console.log(err);
+										// });
+									}
+								});
+							}}
+						>
+							成團
+						</button>
+					{/if}
+				</summary>
+
+				<table class="w-full border-collapse">
+					<thead>
+						<tr>
+							<th class="border border-black">#</th>
+							<th class="border border-black">姓名</th>
+							<th class="border border-black">信箱</th>
+						</tr>
+					</thead>
+					<tbody class="text-center">
+						{#each [event.owner, ...event.members] as member, i}
+							<tr>
+								<td class="border border-black">{i + 1}</td>
+								<td class="border border-black">{member.name}</td>
+								<td class="border border-black">{member.email}</td>
+							</tr>
+						{/each}
+					</tbody>
+				</table>
+			</details>
+		{:else}
+			<div>當前參與人數：{event.members_count + 1}人</div>
+		{/if}
+	</div>
+
 	<div class="text-gray-400">
 		{new Date(startDate).toLocaleString()}
 		{#if event.end_time}
@@ -168,9 +240,29 @@
 	<div class="border-b border-gray-200 dark:border-gray-700" />
 </div>
 
+<div class="mx-8 rounded-md border p-4 md:mx-36">
+	<h1 class="text-xl font-bold">發起人</h1>
+	<div class="flex items-center">
+		<img
+			class="h-10 w-10 rounded-full bg-gray-200 object-cover object-center"
+			src={event.owner.avatar}
+			alt=""
+		/>
+		<div class="ml-4">
+			<div class="text-sm font-medium text-gray-900">
+				{event.owner.name}
+			</div>
+			<div class="text-sm text-gray-500">
+				{event.owner.email}
+			</div>
+		</div>
+	</div>
+</div>
+
 <div class="m-8 grid-cols-[3fr_2fr] gap-16 md:mx-36 xl:grid">
-	<div class="mb-12 flex flex-col gap-6">
-		<div class="mt-4 whitespace-pre-wrap leading-8">
+	<div class="mb-4 flex flex-col gap-6">
+		<h1 class="text-2xl font-bold">活動內容</h1>
+		<div class="whitespace-pre-wrap leading-8">
 			{event.description}
 		</div>
 	</div>
@@ -243,6 +335,20 @@
 						type="submit">Comment</button
 					>
 				</form>
+			</div>
+		{:else}
+			<div class="mx-auto mb-4 mt-2 flex max-w-3xl flex-col items-center rounded-lg shadow">
+				<textarea
+					rows="4"
+					class="block w-full rounded-lg border border-gray-300 bg-gray-100 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+					placeholder="You must be a member to comment"
+					disabled
+				/>
+				<button
+					class="mt-2 inline-flex w-full items-center justify-center rounded-lg bg-green-700 bg-opacity-50 px-3 py-2 text-center text-sm font-medium text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+					type="submit"
+					disabled>Comment</button
+				>
 			</div>
 		{/if}
 	</div>
